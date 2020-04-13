c19b001aa40fdd97dca70f8062024fe20aa32de6
James Johnson <jtaylorjohnson@gmail.com>
2020-04-13 20:22:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -41,3 +41,4 @@ binding.pry
 # html = Nokogiri::HTML(open(https://learn-co-curriculum.github.io/student-scraper-test-page/students/ryan-johnson.html))
 # html.css("social-icon-container").first.value
 end
+html.css("d.social-icon-containera").first
