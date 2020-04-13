29ccfb4afeaaabf550b86642bae4b28bb8d68f45
James Johnson <jtaylorjohnson@gmail.com>
2020-04-13 20:30:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -35,8 +35,8 @@ class Scraper
         student_profile[:blog] = social.attribute("href").value
       end
     end
-    student_profile[:profile_quote] = profile.css("div.main-wrapper.profile .vitals-text-container .profile-quote").text
-    student_profile[:bio] = profile.css("div.main-wrapper.profile .description-holder p").text
+    student_profile[:profile_quote] = html.css("div.main-wrapper.profile .vitals-text-container .profile-quote").text
+    student_profile[:bio] = html.css("div.main-wrapper.profile .description-holder p").text
 
     student_profile
   end
